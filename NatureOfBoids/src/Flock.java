ed98e492bf6869539d43ec71113b0d3ed70dd9d1
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2020-02-16 03:18:46 +0300

    Add files via upload

---
+++ NatureOfBoids/src/Flock.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Flock
+{
+    public static ArrayList<Boid> boids = new ArrayList<>();
+    public Flock()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            boids.add(new Boid());
+        }
+    }
+    public void setRandomVel()
+    {
+        for (Boid boid: boids)
+        {
+            boid.setRandVel();
+        }
+    }
+}
